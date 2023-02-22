feced6f6fcbe737eb3b08fc5a357de84d6bc82b5
2815893479@qq.com <2815893479@qq.com>
2023-02-23 00:01:58 +0800

    Update

--- Hospital-Self-service-Machines/PageOne.aspx
+++ Hospital-Self-service-Machines/PageOne.aspx
@@ -1,6 +1,31 @@
 ﻿<%@ Page Title="首页" Language="C#" MasterPageFile="~/母版页/PageSite.Master" AutoEventWireup="true" CodeBehind="PageOne.aspx.cs" Inherits="Hospital_Self_service_Machines.PageOne" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+/*    .auto-style1 {
+        height: 620px;
+        width: 1435px;
+        float:left;
+    }*/
+    .btnSize{
+        height:200px;
+        width:300px;
+        border-radius:20px;
+    }
+</style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
+    <table style="float:right">
+        <tr>
+            <td><asp:Button runat="server" ID="btn_Registered" Text="挂号" BackColor="#ffcc00" CssClass="btnSize" /></td>
+            <td><asp:Button runat="server" ID="btn_BulidInfo" Text="建档" BackColor="#66ff66" CssClass="btnSize"/></td>
+        </tr>
+        <tr>
+            <td><asp:Button runat="server" ID="btn_Payment" Text="缴费" BackColor="#66ffcc" CssClass="btnSize" /></td>
+            <td><asp:Button runat="server" ID="btn_AppointmentRegistration" Text="预约挂号" BackColor="#3399ff" CssClass="btnSize"/></td>
+        </tr>
+        <tr>
+            <td><asp:Button runat="server" ID="btn_TakeNumber" Text="取号" BackColor="#ff9933" CssClass="btnSize" /></td>
+            <td><asp:Button runat="server" ID="btn_OtherFunction" Text="其它" BackColor="#00ff00" CssClass="btnSize"/></td>
+        </tr>
+    </table>
 </asp:Content>
--- Hospital-Self-service-Machines/PageOne.aspx.designer.cs
+++ Hospital-Self-service-Machines/PageOne.aspx.designer.cs
@@ -13,5 +13,59 @@ namespace Hospital_Self_service_Machines
 
     public partial class PageOne
     {
+
+        /// <summary>
+        /// btn_Registered 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Registered;
+
+        /// <summary>
+        /// btn_BulidInfo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_BulidInfo;
+
+        /// <summary>
+        /// btn_Payment 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Payment;
+
+        /// <summary>
+        /// btn_AppointmentRegistration 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_AppointmentRegistration;
+
+        /// <summary>
+        /// btn_TakeNumber 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_TakeNumber;
+
+        /// <summary>
+        /// btn_OtherFunction 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_OtherFunction;
     }
 }
