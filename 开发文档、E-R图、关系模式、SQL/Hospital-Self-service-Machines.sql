bcad42c7c0054fb82bf335f5b2d2706c4949a156
2815893479@qq.com <2815893479@qq.com>
2023-02-27 00:30:30 +0800

    update

--- 开发文档、E-R图、关系模式、SQL/Hospital-Self-service-Machines.sql
+++ 开发文档、E-R图、关系模式、SQL/Hospital-Self-service-Machines.sql
@@ -10,6 +10,7 @@ CREATE DATABASE 医院自助服务机
 GO
 
 USE 医院自助服务机
+
 	IF OBJECT_ID('tb_User')IS NOT NULL
 		DROP TABLE tb_User;
 	GO
@@ -28,3 +29,26 @@ USE 医院自助服务机
 		)
 	INSERT INTO tb_User(UserNo,UserName,Password) VALUES
 	('3210707010','李健铭',HASHBYTES('MD2','qwe123456'))
+
+	IF OBJECT_ID('tb_Department')IS NOT NULL
+		DROP TABLE tb_Department;
+	GO
+	CREATE TABLE tb_Department
+		(DepartmentNo
+			CHAR(10)
+			NOT NULL
+			CONSTRAINT DepartmentNo
+				PRIMARY KEY(DepartmentNo)
+		,DepartmentName
+			VARCHAR(40)
+			NOT NULL
+		)
+	INSERT INTO tb_Department(DepartmentNo,DepartmentName) VALUES
+	('00','---请选择---'),
+	('01','内科'),
+	('02','外科'),
+	('03','五官科'),
+	('04','妇产科'),
+	('05','皮肤性病科'),
+	('06','精神心理科'),
+	('07','其它科室')
